b692b436555522a91872cc92e329e4f90241f7d6
Bulkan Evcimen <bulkan@gmail.com>
2012-08-17 17:20:16 +1000

    working on index view that github will call
    * plus the tests using WebTest

--- app/github.py
+++ app/github.py
@@ -1,10 +1,23 @@
-from bottle import route, run
+import json
+
+from bottle import route, run, request, response
+
+import bottle
 
 
-@route('/')
-def hello_worl():
-    return 'Hello'
+@route('/', method='POST')
+def index():
+    payload = request.POST.get('payload')
+    if not payload:
+        response.status = "500 no payload found"
+        return response
 
+    payload = json.loads(payload)
+
+    return 'OK'
+
+
+app = bottle.default_app()
 
 if __name__ == '__main__':
     run()
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,9 +1,29 @@
-import sys
+import json
 
+import unittest
+
+import sys
+# add app to pp
 sys.path.append('../')
+
+from webtest import TestApp
 
 from app import github
 
 
-def test_app_index():
-    print github.hello_worl()
+class ServerTests(unittest.TestCase):
+
+    def test_app_index(self):
+        app = TestApp(github.app)
+        payload = open('tests/payload').read()
+        resp = app.post('/', {'payload': payload})
+        self.assertEqual("OK", resp.body)
+
+    def test_status_codes(self):
+        app = TestApp(github.app)
+
+        # get not allowed
+        app.get('/', status=405)
+
+        # post without payload should 500
+        app.post('/', status=500)
